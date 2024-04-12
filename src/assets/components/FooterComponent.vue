7aec8f33100d09df23a179c8b402329e24450b8c
Federica Gallo <gallofederica58@gmail.com>
2024-04-12 17:11:10 +0200

    v-for

--- src/assets/components/FooterComponent.vue
+++ src/assets/components/FooterComponent.vue
@@ -1,18 +1,41 @@
 <template>
-   <footer>
-    <ul>
-<li>Federica</li>
-<li>Elisa</li>
-<li>ANdrea</li>
-<li>Adriana</li>
-<li>Francesca</li>
-   </ul></footer>
+    <footer>
+        <ul>
+            <li>Federica</li>
+            <li>Elisa</li>
+            <li>ANdrea</li>
+            <li>Adriana</li>
+            <li>Francesca</li>
+        </ul>
+        <ul>
+            <li v-for="(todo, index) in listToDo" :key="index">{{todo.where + todo.when + todo.what}}</li>
+        </ul>
+    </footer>
 </template>
 <script>
-    export default {
-        name: 'FooterComponent',
-        
+export default {
+    name: 'FooterComponent',
+    data() {
+        return {
+            listToDo: [
+                {
+                    where: London,
+                    when: April,
+                    what: drink
+                },
+                {
+                    where: London,
+                    when: April,
+                    what: drink
+                },
+                {
+                    where: London,
+                    when: April,
+                    what: drink
+                }
+            ]
+        }
     }
+}
 </script>
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
