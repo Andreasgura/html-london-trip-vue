503c4f32b3a2bcb44a65f6b60d6c1da96a29140f
ElisaMavilia <elisamavilia1@gmail.com>
2024-04-12 16:17:03 +0200

    add images

---
+++ src/assets/components/MainComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <main>
+        main
+        <div id="jumbotron">
+            <img src="../img/pexels-bill-emrich-230794-0.jpg" alt="London">
+            <img src="../img/pexels-bill-emrich-230794-1.jpg" alt="London">
+            <img src="../img/pexels-bill-emrich-230794-2.jpg" alt="London">
+        </div>
+       
+    </main>
+</template>
+
+<script>
+    export default {
+        name: "MainComponent"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
