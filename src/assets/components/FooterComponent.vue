82339711e7b3203e7ad85af3fbb176aeaf1c87bf
Federica Gallo <gallofederica58@gmail.com>
2024-04-12 16:46:55 +0200

    list of names

---
+++ src/assets/components/FooterComponent.vue
@@ -0,0 +1,17 @@
+<template>
+   <footer>
+    <ul>
+<li>Federica</li>
+<li>Elisa</li>
+<li>ANdrea</li>
+<li>Adriana</li>
+   </ul></footer>
+</template>
+<script>
+    export default {
+        name: 'FooterComponent',
+        
+    }
+</script>
+<style lang="scss" scoped>
+</style>
